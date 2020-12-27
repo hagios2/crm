c15bbdfbf9b0e52c061321957993e86637bc6cfb
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-12-27 00:09:52 +0000

    added dynamic urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (home, customers, products)
+from .views import (home, customers, products, createOrder)
 
 urlpatterns = [
     path('dashboard/', home),
     path('customer/<str:pk_text>', customers),
     path('products/', products),
-
+    path('orders/create/', createOrder, name='order_create'),
 
 ]
